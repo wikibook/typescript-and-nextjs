274b6e1e9867e7b4b83dd2864fbd32fee0823e46
moseskim <creatinov.kim@gmail.com>
2023-04-01 21:22:19 +0900

    정오표 반영
    `https://gihyo.jp/book/2022/978-4-297-12916-3/support#supportApology` 반영

--- ch03/react-sample/src/components/ContainerSample.tsx
+++ ch03/react-sample/src/components/ContainerSample.tsx
@@ -1,30 +1,27 @@
-// Container의 props 타입을 정의한다
+import React from 'react'
+
+// React17 이전에는 FC를 지정한 경우,
+// children이 props에 암묵저긍로 포함된다
 type ContainerProps = {
   title: string
-  children: React.ReactNode
 }
 
-// React 컴포넌트의 타입 붙이기에 관해서는 아래를 참조한다
-// https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/function_components/
-const Container = (props: ContainerProps): JSX.Element => {
+const Container: React.FC<ContainerProps> = (props) => {
   const { title, children } = props
 
   return (
     <div style={{ background: 'red' }}>
       <span>{title}</span>
-      {/* props의 children을 삽입하면, 이 컴포넌트의 시작 태그와 종료 태그로 감싼 요소를 표시한다 */}
       <div>{children}</div>
     </div>
   )
 }
 
-const Parent = (): JSX.Element => {
+// React17 이전에는 children를 사용하지 않는 경우 VFC를 지정한다
+const Parent: React.VFC = () => {
   return (
-    // Container를 사용할 때, 다른 요소를 감싸서 사용한다
-    <Container title="Hello">
-      <p>ここの部分が背景色で囲まれます</p>
+    <Container>
+      <p>이 부분은 배경색으로 둘러 싸여 있습니다.</p>
     </Container>
   )
 }
-
-export default Parent
